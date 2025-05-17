e40bdc236058a4f9fcb6b4d2b4cd8d7660b3e4ec
Mariuxi <catmaluci@hotmail.com>
2025-05-17 17:47:01 +0200

    fix cors config for api/me

--- src/main/java/com/mariu/catalog/config/CorsConfig.java
+++ src/main/java/com/mariu/catalog/config/CorsConfig.java
@@ -17,9 +17,9 @@ public class CorsConfig {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        String[] origins = allowedOrigins.split("");
+        String[] origins = allowedOrigins.split(",");
 
-        registry.addMapping("/**")
+        registry.addMapping("/*")
             .allowedOrigins(origins)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowCredentials(true);
